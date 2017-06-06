33d1dfd6ea53792c53b43a6a7bf0551c2b2ab46c
Jyoti Sutar <jyoti.sutar92@gmail.com>
2017-06-06 14:38:25 +0530

    Add files via upload

---
+++ paths.sh
@@ -0,0 +1,26 @@
+# path for samtools
+samtools="/usr/local/bin/samtools"
+# path for fastq quality filter tool of fastx toolkit
+fastqqf="/usr/local/bin/fastq_quality_filter"
+# path for fasta formatter tool of fastx toolkit
+fastaf="/usr/local/bin/fasta_formatter"
+# path for transeq tool of emboss package
+transeq="/usr/bin/transeq"
+# hmmer path
+hmmsearch="/usr/bin/hmmsearch"
+# path for the hmm profile to be used for frame correction
+hmmprofile="/home/bofa/BOFA/NFLG_NGSDA/gp41Alignments/Batch1Analysis/Analysis/gp41part.hmm"
+# path for muscle
+muscle="/usr/bin/muscle"
+# path for extractalign tool of emboss package
+extractalign="/usr/bin/extractalign"
+
+export samtools
+export fastqqf
+export fastaf
+export transeq
+export hmmsearch
+export hmmprofile
+export muscle
+export extractalign
+
